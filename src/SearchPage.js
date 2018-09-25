b306ed42244679ba6290e4a96aa06505385d2c31
Hari krishnan <hari4125862@gmail.com>
2018-09-25 15:02:06 +0530

    Update SearchPage.js
    updated the shelf at search page

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,57 +1,72 @@
-import React,{Component} from 'react';
-import Book from './Book';
-import * as BooksAPI from './BooksAPI'
-import {Link} from 'react-router-dom' ;
-import {debounce} from 'lodash';
+import React, { Component } from "react";
+import * as BooksAPI from "./BooksAPI";
+import { Link } from "react-router-dom";
+import { debounce } from "lodash";
+import Book from "./Book.js";
 
-class SearchPage extends Component{
-constructor(props){
-super(props);
-this.state={
-query:'',
-searchedBooks:[]
-}
-}
-updateQuery= debounce(query =>{
-this.setState({
-query: query
-});
-this.updateSearchedBooks(query);
-},1000);
-
-updateSearchedBooks=(query)=>{
-if(query){
-BooksAPI.search(query).then((searchedBooks) =>{
-if(searchedBooks.error)
-this.setState({searchedBooks:[]});
-else if(query==this.state.query)
-this.setState({searchedBooks: searchedBooks});
-})
-} else
-this.setState({searchedBooks:[]});
-}
-
-render(){
-return(
-<div className="search-books">
-    <div className="search-books-bar">
-        <Link to="/" className="close-search">Close</Link>
-        <div className="search-books-input-wrapper">
-            <input type="text" placeholder="Search by title or author" onChange={(event)=>
-            this.updateQuery(event.target.value)}/>
+class SearchPage extends Component {
+  state = {
+    query: "",
+    searchedBooks: []
+  };
+  updateQuery = debounce(query => {
+    this.setState({ query: query });
+    this.updateSearchedBooks(query);
+  }, 1000);
+  updateSearchedBooks = query => {
+    if (query) {
+      BooksAPI.search(query).then(searchedBooks => {
+        if (searchedBooks.error) {
+          this.setState({ searchedBooks: [] });
+        } else {
+          this.setState({ searchedBooks });
+        }
+      });
+    } else {
+      this.setState({ searchedBooks: [] });
+    }
+  };
+  render() {
+    return (
+      <div className="search-books">
+        <div className="search-books-bar">
+          <Link className="close-search" to="/">
+            Close
+          </Link>
+          <div className="search-books-input-wrapper">
+            <input
+              type="text"
+              placeholder="Search by title or author"
+              // value={this.state.query}
+              onChange={event => {
+                this.updateQuery(event.target.value);
+              }}
+            />
+          </div>
         </div>
-    </div>
-    <div className="search-books-results">
-        <ol className="books-grid">
-            {this.state.searchedBooks.map(searchedBook =>(
-            <li key={searchedBook.id}>
-                <Book book={searchedBook} moveShelf={this.props.moveShelf} />
-            </li>
-            ))}
-        </ol>
-    </div>
-</div>
-);
+        <div className="search-books-results">
+          <ol className="books-grid">
+            {this.state.searchedBooks.map(searchedBook => {
+              let shelf = "none";
+              this.props.books.map(
+                book =>
+                  book.id === searchedBook.id ? (shelf = book.shelf) : null
+              );
+              return (
+                <li key={searchedBook.id}>
+                  <Book
+                    book={searchedBook}
+                    moveShelf={this.props.moveShelf}
+                    currentShelf={shelf}
+                  />
+                </li>
+              );
+            })}
+          </ol>
+        </div>
+      </div>
+    );
+  }
 }
-}
+
 export default SearchPage;
